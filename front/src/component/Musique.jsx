b3055c44c73846b8f3d1b33559ce339c079fbc70
b20anne <b20anne@yahoo.fr>
2021-02-03 15:35:40 +0100

    verification react-app

---
+++ front/src/component/Musique.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Musique () {
+    return(
+        <div className="cadre" >
+            <p>Hello musique</p>
+        </div>
+    );
+}
+
+export default Musique;
